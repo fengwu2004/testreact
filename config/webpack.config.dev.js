378f5f9fb2f8c3b01f9d3438c180db9fab4f4228
晏立 <fengwu2006@gmail.com>
2018-05-29 15:51:58 +0800

    save

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -161,10 +161,12 @@ module.exports = {
             use: [
               require.resolve('style-loader'),
               {
-                loader: require.resolve('css-loader'),
-                options: {
-                  importLoaders: 1,
-                },
+								loader: require.resolve('css-loader'),
+								options: {
+									importLoaders: 1,
+									modules: true,
+									localIdentName: "[name]__[local]___[hash:base64:5]"
+								},
               },
               {
                 loader: require.resolve('postcss-loader'),
--- src/views/HistoryCell.js
+++ src/views/HistoryCell.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './HistoryCell.css'
+import style from './HistoryCell.css'
 
 import reserve from '../assets/reserve.png'
 import gotoparking from '../assets/gotoparking.png'
@@ -11,29 +11,29 @@ export class HistoryCell extends Component{
 		const {parkingName, money, numberPlate, valid, reserveTime, reserveCarport, recommondTime} = this.props.reserveInfo
 		
 		return (
-			<div className='card'>
-				<div className='cellitem'>
-					<span className='parkingName'>{parkingName}</span><span>{money + '元'}</span>
+			<div className={style.card}>
+				<div className={style.cellitem}>
+					<span className={style.parkingName}>{parkingName}</span><span>{money + '元'}</span>
 				</div>
-				<div className='cellitem'>
-					<span className='numberPlate'>{numberPlate}</span><span className='valid'>{valid?'未过期':'已过期'}</span>
+				<div className={style.cellitem}>
+					<span className={style.numberPlate}>{numberPlate}</span><span className={style.valid}>{valid?'未过期':'已过期'}</span>
 				</div>
-				<div className='cellitem'>
-					<span className='reserveTime'>{'预定时间' + reserveTime}</span><span className='reserveTime'>{'预定车位:' + reserveCarport}</span>
+				<div className={style.cellitem}>
+					<span className={style.reserveTime}>{'预定时间' + reserveTime}</span><span className={style.reserveTime}>{'预定车位:' + reserveCarport}</span>
 				</div>
-				<div className='cellitem'>
-					<div className='reserveTime' style={{color: '#C8C8C8'}}>{'请在' + new Date(recommondTime).toLocaleDateString() + '前入场' }</div>
+				<div className={style.cellitem}>
+					<div className={style.reserveTime} style={{color: '#C8C8C8'}}>{'请在' + new Date(recommondTime).toLocaleDateString() + '前入场' }</div>
 				</div>
-				<div className='line'></div>
-				<div className='btns'>
-					<div className='btn'>
-						<img className='btn-icon' src={reserve} width={15} alt='link'></img>
-						<span className='btn-text'>预定车位引导</span>
+				<div className={style.line}></div>
+				<div className={style.btns}>
+					<div className={style.btn}>
+						<img className={style.btn_icon} src={reserve} width={15} alt='link'></img>
+						<span className={style.btn_text}>预定车位引导</span>
 					</div>
-					<div className='verticalline'></div>
-					<div className='btn'>
-						<img className='btn-icon' src={gotoparking} width={15} alt='link'></img>
-						<span className='btn-text'>前往停车场</span>
+					<div className={style.verticalline}></div>
+					<div className={style.btn}>
+						<img className={style.btn_icon} src={gotoparking} width={15} alt='link'></img>
+						<span className={style.btn_text}>前往停车场</span>
 					</div>
 				</div>
 			</div>
